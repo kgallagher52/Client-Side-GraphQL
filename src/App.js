598adc71cf762c726a68760dbd78ce355a754d81
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-04-26 19:46:48 -0600

    finished apollo configuration for the application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+// Packages
 import React from 'react';
+import ApolloClient from 'apollo-boost'; // Apollo Boost is just a bundle of necessary packages for apollo client
+import { ApolloProvider } from 'react-apollo'; //Binds Apollo to react wraps our application
+//Components
 import BookList from './components/BookList';
+
+//Style Sheets
 import './styles/index.css';
+
+//Apollo Configuration 
+const client = new ApolloClient({
+  url:'http://localhost:5000/graphql'
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>Book Reading List</h1>
-      <BookList/>
-    </div>
+    //client={client} tells apollo provider what client to hook to.
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Book Reading List</h1>
+        <BookList/>
+      </div>
+    </ApolloProvider>
+
   );
 }
 
